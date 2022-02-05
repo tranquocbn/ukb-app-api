8e316a29bedbd9664fb0c970613fc5f4632ce341
Tran Quoc <quoctk@deha-soft.com>
2022-02-05 17:06:38 +0700

    Refactor code và viết logic cho chức năng login

---
+++ app/Services/BaseService.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Services;
+use Illuminate\Http\Response;
+
+class BaseService
+{
+    protected function resSuccessOrFail(array $data = null, string $msg, int $status = Response::HTTP_OK)
+    {
+        $dataRes = [
+            'message' => $msg,
+            'status' => $status
+        ];
+
+        if($data !== null) {
+            $dataRes['data'] = $data;
+        }
+        return response()->json($dataRes, $status);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -16,7 +16,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    echo 'Dang nhap vs token';
     return $request->user();
 });
 
